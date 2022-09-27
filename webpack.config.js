6a0bfbd425d7b56ff16117905bd9d789153cfd37
ChrisWen <chriswen96.com>
2022-09-27 17:47:48 +0800

    配置webpack.config.js

---
+++ webpack.config.js
@@ -0,0 +1,20 @@
+const path = require("path");
+module.exports = {
+  mode: "production",
+  entry: {
+    index: "./lib/index.tsx",
+  },
+  output: {
+    path: path.resolve(__dirname, "dist/lib"),
+    library: "FUI", //输出的文件名
+    libraryTarget: "umd", //输出的文件格式，即library在全部环境都可以使用
+  },
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        loader: "awesome-typescript-loader",
+      },
+    ],
+  },
+};
