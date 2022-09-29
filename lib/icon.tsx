c1ea0d594353e190f4a27b2fd832c2b08312c19f
ChrisWen <chriswen96.com>
2022-09-29 11:23:29 +0800

    svg:<use xlinkHref={`#${props.name}`}

--- lib/icon.tsx
+++ lib/icon.tsx
@@ -1,6 +1,9 @@
 import React from 'react';
-import wechat from './icons/wechat.svg';
-console.log(wechat);
+import './icons/wechat.svg';
+import './icons/alipay.svg';
+import './icons/qq.svg';
+
+
 interface IconProps {
   name: string;
 }
@@ -9,8 +12,17 @@ const Icon: React.FunctionComponent<IconProps> = (props) => {
   //Icon 是一个react的函数组件，他的属性类型是IconProps（里面有name:string）
   return (
     <span>
+      {/* <svg>
+        <use xlinkHref="#wechat"></use>
+      </svg>
       <svg>
-        <use xlinkHref={`#${props.name}`}/>
+        <use xlinkHref="#alipay"></use>
+      </svg>
+      <svg>
+        <use xlinkHref="#qq"></use>
+      </svg> */}
+      <svg>
+        <use xlinkHref={`#${props.name}`} />
       </svg>
     </span>
   );
--- lib/index.tsx
+++ lib/index.tsx
@@ -1,7 +1,20 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from "react";
 import Icon from './icon'
-ReactDOM.render(<div>
-  <Icon name="wechat"></Icon>
-  </div>,document.body
-);
+
+import {createRoot} from 'react-dom/client'
+
+// ReactDOM.render(<div>
+//   <Icon name="wechat"></Icon>
+//   </div>,document.querySelector('#root')
+// );
+
+const container = document.getElementById('root');
+const root = createRoot(container) ;
+root.render(<div>
+  <Icon name="wechat" />
+  <Icon name="alipay" />
+  <Icon name="qq" />
+</div>
+)
+
+
